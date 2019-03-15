6d0c40dbe936c22f232d1fe8bfc214e687ac5b88
Isaac Omondi <iomondi90@gmail.com>
2019-03-15 11:29:56 +0300

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+
+export const eventBus = new Vue();
 
 new Vue({
   render: h => h(App),
