2b728fc1638472fe8a196317dee7f0b1542118a6
Isaac Omondi <iomondi90@gmail.com>
2019-03-22 13:52:13 +0300

    learning about directives and how to create custom ones

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,61 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <button class="btn btn-default" @click="selectedComponent = 'appQuote'">Quote</button>
-        <button class="btn btn-default" @click="selectedComponent = 'appAuthor'">Author</button>
-        <button class="btn btn-default" @click="selectedComponent = 'appNew'">New</button>
-        <p>{{selectedComponent}}</p>
-        <hr>
-        <keep-alive>
-          <component :is="selectedComponent">Default Content</component>
-        </keep-alive>
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>Built in Directives</h1>
+        <p v-text="'Some Text'"></p>
+      </div>
+    </div>
+    <hr>
 
-        <!-- <app-quote>
-          <h1>{{name}}</h1>
-        </app-quote>-->
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color this!</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay:500}">Color this!</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-import Author from "./components/Author.vue";
-import New from "./components/New.vue";
-
 export default {
-  data: function() {
-    return {
-      name: "Isaac",
-      selectedComponent: "appQuote"
-    };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+
+        if (binding.modifiers["delayed"]) {
+          delay = binding.value.delay;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 Vue.config.productionTip = false
 import 'bootstrap/dist/css/bootstrap.css'
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+
+  }
+})
 
 new Vue({
   render: h => h(App),
