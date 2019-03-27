62eba30981e3931b48fd6add8b35c649b5c094f7
Isaac Omondi <iomondi90@gmail.com>
2019-03-27 14:48:32 +0300

    handling http requests using vue-resources

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,26 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-        <h1>Animations</h1>
-        <hr>
-        <br>
-        <select v-model="alertAnimation" class="form-control">
-          <option value="fade">Fade</option>
-          <option value="slide">Slide</option>
-        </select>
-        <br>
-        <br>
-        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-        <br>
-        <br>
-        <transition :name="alertAnimation">
-          <div class="alert alert-info" v-if="show">Fade Transition</div>
-        </transition>
+      <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label for>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
 
-        <transition :name="alertAnimation">
-          <div class="alert alert-info" v-if="show">Slide Transition</div>
-        </transition>
-
-        <transition enter-active-class="animated bounce" leave-active-class="animated shake">
-          <div class="alert alert-info" v-if="show">Animated Transition</div>
-        </transition>
-
-        <transition :name="alertAnimation" mode="out-in">
-          <div class="alert alert-info" v-if="show" key="info">Fade Transition</div>
-          <div class="alert alert-warning" v-else key="warning">Warning Transition</div>
-        </transition>
+        <div class="form-group">
+          <label for>Email</label>
+          <input type="email" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
 
         <hr>
-        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
-        <br>
-        <br>
-        <transition
-          @before-enter="beforeEnter"
-          @enter="enter"
-          @after-enter="afterEnter"
-          @enter-cancelled="enterCancelled"
-          @before-leave="beforeLeave"
-          @leave="leave"
-          @after-leave="afterLeave"
-          @leave-cancelled="leaveCancelled"
-          :css="false"
-        >
-          <div style="width:100px; height:100px; background-color:lightgreen" v-if="load"></div>
-        </transition>
-
-        <hr>
-
-        <button
-          class="btn btn-primary"
-          @click="selectedComponent == 'success-alert' ? selectedComponent = 'danger-alert' : selectedComponent = 'success-alert'"
-        >Toggle component</button>
-        <br>
-        <br>
-        <transition name="fade" mode="out-in">
-          <component :is="selectedComponent"></component>
-        </transition>
-
-        <hr>
-        <button class="btn btn-primary" @click="addItem">Add Item</button>
+        <input type="text" class="form-control" v-model.lazy="node"> <br><br>
+        <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <br>
         <br>
         <ul class="list-group">
-          <transition-group name="slide">
-            <li
-              class="list-group-item"
-              v-for="(number, index) in numbers"
-              :key="index"
-              @click="removeItem(index)"
-              style="cursor:pointer"
-            >{{number}}</li>
-          </transition-group>
+          <li class="list-group-item" v-for="u in users" :key="u.id">{{u.username }} - {{u.email}}</li>
         </ul>
       </div>
     </div>
@@ -82,128 +28,79 @@
 </template>
 
 <script>
-import DangerAlert from "./components/DangerAlert";
-import SuccessAlert from "./components/SuccessAlert";
-
 export default {
   data() {
     return {
-      show: false,
-      load: false,
-      alertAnimation: "fade",
-      elementWidth: 100,
-      selectedComponent: "success-alert",
-      numbers: [1, 2, 3, 4, 5]
+      user: {
+        username: "",
+        email: ""
+      },
+      users: [],
+      resource: {},
+      node: "users"
     };
   },
   methods: {
-    beforeEnter(el) {
-      console.log("beforeEnter");
-      this.elementWidth = 100;
-      el.style.width = this.elementWidth + "px";
+    submit() {
+      // this.$http
+      //   .post("users.json", this.user)
+      //   .then(
+      //     response => {
+      //       console.log(response);
+      //     },
+      //     error => {
+      //       console.log(error);
+      //     }
+      //   );
+      // this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
     },
-    enter(el, done) {
-      console.log("enter");
-      let round = 1;
-      const interval = setInterval(() => {
-        el.style.width = this.elementWidth + round * 10 + "px";
-        round++;
-        if (round > 20) {
-          clearInterval(interval);
-          done();
-        }
-      }, 20);
-    },
-    afterEnter(el) {
-      console.log("afterEnter");
-    },
-    enterCancelled(el) {
-      console.log("enterCancelled");
-    },
-    beforeLeave(el) {
-      console.log("beforeLeave");
-      el.style.width = "300px";
-    },
-    leave(el, done) {
-      console.log("leave");
-      let round = 1;
-      const interval = setInterval(() => {
-        el.style.width = this.elementWidth - round * 10 + "px";
-        round++;
-        if (round > 20) {
-          clearInterval(interval);
-          done();
-        }
-      }, 20);
-    },
-    afterLeave(el) {
-      console.log("leaveEnter");
-    },
-    leaveCancelled(el) {
-      console.log("leaveCancelled");
-    },
-    addItem() {
-      const pos = Math.floor(Math.random() * this.numbers.length);
-      this.numbers.splice(pos, 0, this.numbers.length + 1);
-    },
-    removeItem(index) {
-      this.numbers.splice(index, 1);
+    fetchData() {
+      // this.$http
+      //   .get("users.json")
+      //   .then(
+      //     response => {
+      //       return response.json();
+      //     },
+      //     error => {
+      //       console.log(error);
+      //     }
+      //   )
+      //   .then(data => {
+      //     const resultArray = [];
+      //     for (let key in data) {
+      //       resultArray.push(data[key]);
+      //     }
+      //     this.users = resultArray;
+      //   });
+      this.resource
+        .getData({ node: this.node })
+        .then(
+          response => {
+            return response.json();
+          },
+          error => {
+            console.log(error);
+          }
+        )
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
     }
   },
-  components: {
-    dangerAlert: DangerAlert,
-    successAlert: SuccessAlert
+  created() {
+    const customActions = {
+      saveAlt: { method: "POST", url: "admins.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource("{node}.json", {}, customActions);
   }
 };
 </script>
 
 <style>
-.fade-enter {
-  opacity: 0;
-}
-.fade-enter-active {
-  transition: opacity 1s;
-}
-.fade-leave {
-  /* opacity: 1; */
-}
-.fade-leave-active {
-  transition: opacity 1s;
-  opacity: 0;
-}
-
-.slide-enter {
-  opacity: 0;
-}
-.slide-enter-active {
-  animation: slide-in 1s ease-out forwards;
-  transition: opacity 1s;
-}
-.slide-leave {
-}
-.slide-leave-active {
-  animation: slide-out 1s ease-out forwards;
-  transition: opacity 1s;
-  opacity: 0;
-  position: absolute;
-}
-.slide-move {
-  transition: transform 1s;
-}
-@keyframes slide-in {
-  from {
-    transform: translateY(20px);
-  }
-  to {
-    transform: translateY(0);
-  }
-}
-@keyframes slide-out {
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(20px);
-  }
-}
 </style>
