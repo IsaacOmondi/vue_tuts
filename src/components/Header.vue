7759f4198c3b75db059abad1cafd39b28c27a30f
Isaac Omondi <iomondi90@gmail.com>
2019-03-28 13:21:04 +0300

    navigate using router links

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link to="/" class="nav-item nav-link" active-class="active" tag="li" exact><a>Home</a> </router-link>
+        <router-link to="/user" class="nav-link" active-class="active" tag="li"><a>User</a> </router-link>
+    </ul>
+</template>
