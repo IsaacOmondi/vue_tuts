181500d2c165d2d9e336693d4eb8981805028b75
Isaac Omondi <iomondi90@gmail.com>
2019-03-28 11:41:24 +0300

    set up vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-        <h1>Http</h1>
-        <div class="form-group">
-          <label for>Username</label>
-          <input type="text" class="form-control" v-model="user.username">
-        </div>
-
-        <div class="form-group">
-          <label for>Email</label>
-          <input type="email" class="form-control" v-model="user.email">
-        </div>
-        <button class="btn btn-primary" @click="submit">Submit</button>
-
-        <hr>
-        <input type="text" class="form-control" v-model.lazy="node"> <br><br>
-        <button class="btn btn-primary" @click="fetchData">Get Data</button>
-        <br>
-        <br>
-        <ul class="list-group">
-          <li class="list-group-item" v-for="u in users" :key="u.id">{{u.username }} - {{u.email}}</li>
-        </ul>
+        <h1>Routing</h1>
       </div>
     </div>
   </div>
@@ -31,73 +12,11 @@
 export default {
   data() {
     return {
-      user: {
-        username: "",
-        email: ""
-      },
-      users: [],
-      resource: {},
-      node: "users"
+      
     };
   },
   methods: {
-    submit() {
-      // this.$http
-      //   .post("users.json", this.user)
-      //   .then(
-      //     response => {
-      //       console.log(response);
-      //     },
-      //     error => {
-      //       console.log(error);
-      //     }
-      //   );
-      // this.resource.save({}, this.user);
-      this.resource.saveAlt(this.user);
-    },
-    fetchData() {
-      // this.$http
-      //   .get("users.json")
-      //   .then(
-      //     response => {
-      //       return response.json();
-      //     },
-      //     error => {
-      //       console.log(error);
-      //     }
-      //   )
-      //   .then(data => {
-      //     const resultArray = [];
-      //     for (let key in data) {
-      //       resultArray.push(data[key]);
-      //     }
-      //     this.users = resultArray;
-      //   });
-      this.resource
-        .getData({ node: this.node })
-        .then(
-          response => {
-            return response.json();
-          },
-          error => {
-            console.log(error);
-          }
-        )
-        .then(data => {
-          const resultArray = [];
-          for (let key in data) {
-            resultArray.push(data[key]);
-          }
-          this.users = resultArray;
-        });
-    }
-  },
-  created() {
-    const customActions = {
-      saveAlt: { method: "POST", url: "admins.json" },
-      getData: { method: "GET" }
-    };
-    this.resource = this.$resource("{node}.json", {}, customActions);
+    
   }
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>The Home Page</h1>
+    <hr>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore porro 
+        recusandae, quisquam consectetur vero, quibusdam animi debitis neque 
+        obcaecati iusto laboriosam mollitia 
+        tempora provident beatae corporis impedit aperiam est dolore?</p>
+  </div>
+</template>
