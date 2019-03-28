bec0daa31eba42bc582a58062186571b2051ac86
Isaac Omondi <iomondi90@gmail.com>
2019-03-28 14:08:33 +0300

    reacting to changes in router parameters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" class="nav-item nav-link" active-class="active" tag="li" exact><a>Home</a> </router-link>
-        <router-link to="/user" class="nav-link" active-class="active" tag="li"><a>User</a> </router-link>
+        <router-link to="/user/1" class="nav-link" active-class="active" tag="li"><a>User 1</a> </router-link>
+        <router-link to="/user/2" class="nav-link" active-class="active" tag="li"><a>User 2</a> </router-link>
+
     </ul>
 </template>
