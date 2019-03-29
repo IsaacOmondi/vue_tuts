0ff10e500d1022b395869f878d96909d5d760564
Isaac Omondi <iomondi90@gmail.com>
2019-03-29 22:05:01 +0300

    refactor: Implementation of child nested routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
     <ul class="nav nav-pills">
-        <router-link to="/" class="nav-item nav-link" active-class="active" tag="li" exact><a>Home</a> </router-link>
-        <router-link to="/user/1" class="nav-link" active-class="active" tag="li"><a>User 1</a> </router-link>
-        <router-link to="/user/2" class="nav-link" active-class="active" tag="li"><a>User 2</a> </router-link>
+        <router-link to="/" class="nav-link" active-class="active" tag="li" exact><a class="nav-item">Home</a> </router-link>
+        <router-link to="/user" class="nav-link" active-class="active" tag="li"><a class="nav-item">User</a> </router-link>
 
     </ul>
 </template>
