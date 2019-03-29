157b8673c8c7b8305301ba3565acec936dfee142
Isaac Omondi <iomondi90@gmail.com>
2019-03-29 22:04:08 +0300

    naming dynamic links

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,3 +1,12 @@
 <template>
+  <div>
     <h3>Some User Details</h3>
+    <router-link
+      tag="button"
+      :to="{name: 'userEdit', params: {
+          id: $route.params.id
+      }}"
+      class="btn btn-primary"
+    >Edit User</router-link>
+  </div>
 </template>
